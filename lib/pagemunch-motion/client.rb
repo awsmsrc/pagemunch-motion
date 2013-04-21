2d1d278a760bdbb3dbec7ea2cde99d51e5842367
Luke <email@luke-roberts.info>
2013-04-21 14:13:04 +0100

    basic working version

--- lib/pagemunch-motion/client.rb
+++ lib/pagemunch-motion/client.rb
@@ -2,6 +2,39 @@ module Pagemunch
   class Client
     def boo
       puts  'boo'
+      puts configuration
+    end
+
+    def summary(url, onComplete:block)
+      query('summary', forUrl:url, onComplete:block)
+    end
+
+    def classify(url, block)
+      query('classify', forUrl:url, onComplete:block)
+    end
+
+    private
+
+    def urlForAction(action, withTargetUrl:targetUrl)
+      "http://api.pagemunch.com/#{configuration[:version]}/#{action}.json?key=#{configuration[:key]}&url=#{targetUrl}"
+    end
+
+    def configuration
+      Pagemunch.configuration
+    end
+
+    def query(action, forUrl:url, onComplete:block)
+      target = urlForAction(action, withTargetUrl:url)
+      Dispatch::Queue.concurrent.async do 
+        json = nil
+        begin
+          json = JSONParser.parse_from_url(target)
+        rescue RuntimeError => e
+          presentError e.message
+        end
+
+        Dispatch::Queue.main.sync { block.call(json) }
+      end
     end
   end
 end
--- lib/pagemunch-motion/configurable.rb
+++ lib/pagemunch-motion/configurable.rb
@@ -1,7 +1,22 @@
-module Motion
-  module Project
-    class Config
-      variable :pagemunch_api_key, :pagemunch_api_version
+module Pagemunch
+  module Configurable
+    class << self
+      def self.keys
+        [:key, :version]
+      end
+
+      attr_accessor *keys
+
+      def self.configure
+        yield Pagemunch::Configurable
+        self
+      end
+
+      def self.configuration
+        Hash[Pagemunch::Configurable.keys.map{|key| [key, Pagemunch::Configurable.instance_variable_get(:"@#{key}")]}]
+      end
     end
   end
+  include Configurable
 end
+
